b0c5a755ce514da5f7da78fd40573ee67b40361d
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2023-08-12 13:42:04 -0400

    Pequenos ajustes

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# sudo visudo
+# ubuntu-lab ALL=(ALL) NOPASSWD: ALL
+# ssh-keygen
+# cat ~/.ssh/id_rsa.pub
+# ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu-lab@10.7.125.172
+# ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu-lab@10.7.125.138
+# ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu-lab@10.7.125.164
+# ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu-lab@10.7.125.179
+# ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu-lab@10.7.125.97
+
 ssh ubuntu-lab@10.7.125.172 '/home/ubuntu-lab/Downloads/nidus/remote.sh 1 config.json node-0'
 ssh ubuntu-lab@10.7.125.138 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-1'
 ssh ubuntu-lab@10.7.125.164 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-2'
