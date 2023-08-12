a78abf2f4fe45d4db2149dab858e0377d99f09f5
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2023-08-12 13:46:14 -0400

    Pequenos ajustes

--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@
 # ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu-lab@10.7.125.179
 # ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu-lab@10.7.125.97
 
-ssh ubuntu-lab@10.7.125.172 '/home/ubuntu-lab/Downloads/nidus/remote.sh 1 config.json node-0'
-ssh ubuntu-lab@10.7.125.138 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-1'
-ssh ubuntu-lab@10.7.125.164 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-2'
-ssh ubuntu-lab@10.7.125.179 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-3'
-ssh ubuntu-lab@10.7.125.97 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-4'
+ssh ubuntu-lab@10.7.125.172 '/home/ubuntu-lab/Downloads/nidus/remote.sh 1 config.json node-0 &'
+ssh ubuntu-lab@10.7.125.138 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-1 &'
+ssh ubuntu-lab@10.7.125.164 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-2 &'
+ssh ubuntu-lab@10.7.125.179 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-3 &'
+ssh ubuntu-lab@10.7.125.97 '/home/ubuntu-lab/Downloads/nidus/remote.sh 5 config.json node-4 &'
