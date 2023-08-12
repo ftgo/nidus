b5da12635920a49d4c0d67ac04201f8f92623321
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2023-08-12 15:16:22 -0400

    update

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ssh ubuntu-lab@10.7.125.172 'sudo nohup pkill -9 python &'
-ssh ubuntu-lab@10.7.125.138 'sudo nohup pkill -9 python &'
-ssh ubuntu-lab@10.7.125.164 'sudo nohup pkill -9 python &'
-ssh ubuntu-lab@10.7.125.179 'sudo nohup pkill -9 python &'
-ssh ubuntu-lab@10.7.125.97 'sudo nohup pkill -9 python &'
+ssh ubuntu-lab@10.7.125.172 'sudo nohup pkill -9 python &' &
+ssh ubuntu-lab@10.7.125.138 'sudo nohup pkill -9 python &' &
+ssh ubuntu-lab@10.7.125.164 'sudo nohup pkill -9 python &' &
+ssh ubuntu-lab@10.7.125.179 'sudo nohup pkill -9 python &' &
+ssh ubuntu-lab@10.7.125.97 'sudo nohup pkill -9 python &' &
