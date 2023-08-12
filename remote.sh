ef95f8c6f842379a2036a62b1669297ef0962a61
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2023-08-12 15:04:53 -0400

    update

--- remote.sh
+++ remote.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-NIDUS_PATH=/home/ubuntu-lab/Downloads/nidus
-# NIDUS_PATH=/home/ftgo/repo/ftgo/nidus
-
-cd ${NIDUS_PATH}
+# cd /home/ftgo/repo/ftgo/nidus
+cd /home/ubuntu-lab/Downloads/nidus
 
 rm -rf nidus_log
 
 sleep ${1}
 
-sudo PATH="${NIDUS_PATH}/.venv/bin:${PATH}" python -m nidus --config=${2} ${3} 1>remote.log 2>&1
+# sudo /home/ftgo/repo/ftgo/nidus/.venv/bin/python -m nidus --config=${2} ${3} 1>remote.log 2>&1
+sudo /home/ubuntu-lab/Downloads/nidus/.venv/bin/python -m nidus --config=${2} ${3} 1>remote.log 2>&1
+
