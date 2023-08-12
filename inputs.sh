a4df4c6f93dae90db613813f5b4f12e83c8d3975
Felipe Toledo G Oliveira <felipetgoliveira@gmail.com>
2023-08-12 16:14:43 -0400

    update

---
+++ inputs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./start.sh
+
+sleep 5
+
+python3 inputs.py
+
+sleep 5
+
+./stop.sh
--- remote.sh
+++ remote.sh
@@ -9,4 +9,3 @@ sleep ${1}
 
 # sudo /home/ftgo/repo/ftgo/nidus/.venv/bin/python -m nidus --config=${2} ${3} 1>remote.log 2>&1
 sudo /home/ubuntu-lab/Downloads/nidus/.venv/bin/python -m nidus --config=${2} ${3} 1>remote.log 2>&1
-
